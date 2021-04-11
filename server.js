2f5086a25b83812e90ada87f4842485dce785155
Glitch (hello-express) <none>
2021-04-11 18:28:51 +0000

    🎈🍄 Checkpoint ./server.js:29620604/6762

--- server.js
+++ server.js
@@ -5,10 +5,37 @@ var options = {
 
 var io = require("socket.io")(server, options);
 
+var players = {};
+
+function Player(id) {
+  this.id = id;
+  this.x = 0;
+  this.y = 0;
+  this.z = 0;
+  this.entity = null;
+}
+
 io.sockets.on("connection", function(socket) {
-  console.log("Client has connected!");
-  socket.on("playerJoined", function(name) {
-    console.log(name);
+  socket.on("initialize", function() {
+    var id = socket.id;
+    var newPlayer = new Player(id);
+    // Creates a new player object with a unique ID number.
+
+    players[id] = newPlayer;
+    // Adds the newly created player to the array.
+
+    socket.emit("playerData", { id: id, players: players });
+    // Sends the connecting client his unique ID, and data about the other players already connected.
+
+    socket.broadcast.emit("playerJoined", newPlayer);
+    // Sends everyone except the connecting player data about the new player.
+  });
+  socket.on("positionUpdate", function(data) {
+    players[data.id].x = data.x;
+    players[data.id].y = data.y;
+    players[data.id].z = data.z;
+
+    socket.broadcast.emit("playerMoved", data);
   });
 });
 
